e775027fef03742bee2dba3e27e49a1ccdef5c3c
Rajinder <rajinder.katal@fisglobal.com>
2019-09-13 15:12:15 +0530

    Reseting everthing:

--- src/main/java/com/example/batch/repository/UsersRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.batch.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.batch.model.Users;
-
-public interface UsersRepository extends JpaRepository<Users, Long> {
-}
